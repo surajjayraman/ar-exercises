3e22470c278c73b0e6c8e8a567eb05f0323525b8
surajjayraman <suraj.jayraman@gmail.com>
2021-10-11 23:04:45 -0400

    Calculate revenue total & average, count of stores> 1M in annual sales:Exercise 5.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_avenue = Store.sum(:annual_revenue)
+puts total_avenue
+
+avg_avenue = Store.average(:annual_revenue)
+puts avg_avenue
+
+stores_over_1mill = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+puts stores_over_1mill

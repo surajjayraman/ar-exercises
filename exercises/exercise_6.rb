6229787e6d45faff22e6a23242b10e832890a1f4
surajjayraman <suraj.jayraman@gmail.com>
2021-10-11 23:15:26 -0400

    Create One-to-many association: Exercise 6.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Suraj", last_name: "Nair", hourly_rate: 60)
+@store1.employees.create(first_name: "Rick", last_name: "Cohen", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Shania", last_name: "Mcdonald", hourly_rate: 60)
+@store2.employees.create(first_name: "Arnold", last_name: "Brown", hourly_rate: 60)
+@store2.employees.create(first_name: "Hensing", last_name: "Hillary", hourly_rate: 60)
